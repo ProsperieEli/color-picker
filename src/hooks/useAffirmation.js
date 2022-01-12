f4cd61e40cee0bf953b601113e525ae419ccbf6c
ProsperieEli <ElijahProsperie@gmail.com>
2022-01-12 15:44:13 -0500

    working assignment

--- src/hooks/useAffirmation.js
+++ src/hooks/useAffirmation.js
@@ -1,3 +1,22 @@
-export default function useAffirmation() {
+import { useState, useEffect } from 'react';
+import useColorPicker from './useColorPicker';
+
+export default function useAffirmation({ bgColor, fgColor }) {
+  const [affirmation, setAffirmation] = useState('');
+
   // TODO: Implement me!
+
+  useEffect(() => {
+    const affirmations = [
+      'Great choice!',
+      'I love that color!',
+      'Looks good!',
+      'What a great color combo!',
+      'Ooh la la, so fancy',
+    ];
+    // Generate a random whole number between 0 and the last index of the array
+    const randomIndex = Math.floor(Math.random() * affirmations.length);
+    setAffirmation(affirmations[randomIndex]);
+  }, [bgColor, fgColor]);
+  return { affirmation };
 }
